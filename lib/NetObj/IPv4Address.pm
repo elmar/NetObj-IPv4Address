aae7d234b8377a41369e0b52b87c1644a232429e
Elmar S. Heeb <elmar@heebs.ch>
2015-04-10 10:38:42 +0200

    creating an object of class NetObj::IPv4Address

--- lib/NetObj/IPv4Address.pm
+++ lib/NetObj/IPv4Address.pm
@@ -5,4 +5,19 @@ package NetObj::IPv4Address;
 
 # ABSTRACT: represent a IPv4 address
 
+use Moo;
+
 1;
+
+=head1 SYNOPSIS
+
+  use NetObj::IPv4Address;
+
+  # constructor
+  my $ip = NetObj::IPv4Address->new('127.0.0.1');
+
+=head1 DESCRIPTION
+
+NetObj::IPv4Address represents IPv4 addresses.
+
+NetObj::IPv4Address is implemented as a Moose style object class (using Moo).
--- t/100-use.t
+++ t/100-use.t
@@ -9,3 +9,9 @@ use lib '../lib';
 use Test::More; END { done_testing; }
 
 BEGIN { use_ok('NetObj::IPv4Address'); }
+
+is(
+    ref(NetObj::IPv4Address->new('127.0.0.1')),
+    'NetObj::IPv4Address',
+    'NetObj::IPv4Address must be a class',
+);
